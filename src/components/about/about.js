86d0b20063314128bc00e53417f25e6e2749d68d
Angel <angel.l.puente@gmail.com>
2023-11-10 19:18:08 -0600

    updates

---
+++ src/components/about/about.js
@@ -0,0 +1,20 @@
+export default function About(){
+    return(
+        <div className="aboutPage">
+            <div className="aboutTitle">
+                <h1>
+
+                </h1>
+            </div>
+            <div className="aboutContent">
+                <div className="aboutItem">
+                <img src=""
+                </div>
+                <div className="aboutItem">
+
+                </div>
+
+            </div>
+        </div>
+    )
+}
--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -10,44 +10,54 @@ import { ReactComponent as Yelp } from "../../icons/yelp.svg";
 export default function Main() {
   return (
     <div className="mainPage">
-      <a href="/contact">
-        <button className="startButton">Contact</button>
-      </a>
-      <div className="coverTitle">
-        <h1>Add title</h1>
-        <p>
-          Lorem ipsum dimsum chim sum Lorem ipsum dimsum chim sumLorem ipsum
-          dimsum chim sum Lorem ipsum dimsum chim sum
-        </p>
-      </div>
-      <img src="../images/1920.jpg" className="coverImage"></img>
-      <div className="contentContainer">
-        <div className="contentField">
-          <div className="contentItem">
-            <Location />
-            <h1>Address</h1>
-            <a>Content Text</a>
-            <a>Content Text</a>
+      <div className="mainPageContainer">
+        <div className="coverTitle">
+          <h1>Add title</h1>
+          <p>
+            Lorem ipsum dimsum chim sum Lorem ipsum dimsum chim sumLorem ipsum
+            dimsum chim sum Lorem ipsum dimsum chim sum
+          </p>
+          <a href="/contact">
+            <button className="startButton">Contact</button>
+          </a>
+        </div>
+        <div className="contentContainer">
+          <div className="contentField">
+            <div className="contentItem">
+              <Location />
+              <h1>Address</h1>
+              <a>Content Text</a>
+              <a>Content Text</a>
+            </div>
+            <div className="contentItem">
+              <Clock />
+              <h1>Hours</h1>
+              <a>Content Text</a>
+              <a>Content Text</a>
+            </div>
+            <div className="contentItem">
+              <Phone />
+              <h1>Call</h1>
+              <a>Content Text</a>
+              <a>Content Text</a>
+            </div>
           </div>
-          <div className="contentItem">
-            <Clock />
-            <h1>Hours</h1>
-            <a>Content Text</a>
-            <a>Content Text</a>
-          </div>
-          <div className="contentItem">
-            <Phone />
-            <h1>Call</h1>
-            <a>Content Text</a>
-            <a>Content Text</a>
+          <div className="contentField">
+            <div className="contentItem">
+              <Facebook />
+              <Instagram />
+              <Yelp />
+            </div>
           </div>
         </div>
-        <div className="contentField">
-          <div className="contentItem">
-            <Facebook/>
-            <Instagram/>
-            <Yelp/>
-          </div>
+        <div className="contentBody">
+          <h1 className="buzz">
+            Buzz Buzz Buzz
+          </h1>
+          <p>
+          Lorem ipsum dimsum chim sum Lorem ipsum dimsum chim sumLorem ipsum
+            dimsum chim sum Lorem ipsum dimsum chim sum
+          </p>
         </div>
       </div>
     </div>
