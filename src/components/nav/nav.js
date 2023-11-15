f6f237aaa65abe3e0bceaccfc268c737907f3107
Angel <angel.l.puente@gmail.com>
2023-11-15 11:46:39 -0600

    fixed logo href link

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -5,7 +5,7 @@ export default function Nav() {
   return (
     <nav class="bg-none navStyle dark:bg-gray-900 fixed w-full z-20 top-0 left-0  dark:border-gray-600">
       <div class=" max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="https://flowbite.com/" class="flex items-center">
+        <a href="/" class="flex items-center">
           <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white sjLogo">
             StopJockin
           </span>
