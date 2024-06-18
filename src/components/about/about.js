6df25e553563b3b097ba2f52c63b3414c8ead0d3
Angel <angel.l.puente@gmail.com>
2024-06-18 17:22:11 -0500

    update about spacing, added carousel

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -1,22 +1,18 @@
-import './about.css'
+import "./about.css";
 import React from "react";
-import  AboutImage from "../../photos/haircut1.jpg"
-import Content from '../../data/content.json'
-
+import Carousel from "../carousel/Carousel";
+import Content from "../../data/content.json";
 
 export default function About() {
   return (
     <div className="aboutPage" id="about">
-      <div className="aboutTitle">
-        <h1>About</h1>
-      </div>
       <div className="aboutContent">
         <div className="aboutItem">
-          <img src={AboutImage} />
+          <Carousel />
         </div>
         <div className="aboutItem">
-        <h1>{Content.aboutHeader}</h1>
-        <p>{Content.about}</p>
+          <h1>{Content.aboutHeader}</h1>
+          <p>{Content.about}</p>
         </div>
       </div>
     </div>
---
+++ src/components/carousel/Carousel.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import "./Carousel.css";
+
+const images = [
+  require("../../photos/haircut1.jpg"),
+  require("../../photos/haircut2.jpg"),
+  require("../../photos/haircut3.jpg"),
+  require("../../photos/haircut4.jpg"),
+  require("../../photos/haircut5.jpg"),
+];
+
+export default function Carousel() {
+  const [current, setCurrent] = useState(0);
+  const length = images.length;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrent((prev) => (prev === length - 1 ? 0 : prev + 1));
+    }, 3000);
+    return () => clearInterval(interval);
+  }, [length]);
+
+  return (
+    <div className="carousel-container relative overflow-hidden">
+      <div
+        className="carousel-inner flex transition-transform duration-1000 ease-in-out"
+        style={{ transform: `translateX(-${current * 100}%)` }}
+      >
+        {images.map((image, index) => (
+          <div className="carousel-item min-w-full" key={index}>
+            <img
+              src={image}
+              className="w-full h-auto"
+              alt={`Slide ${index + 1}`}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
